9157e238f7e1fd38406132f58a26325fb6d43216
earaen <pavikaforlearning@gmail.com>
2023-03-25 19:45:36 +0700

    lab2 react

---
+++ Lab#2 React with MUI Framework/todo_app/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css";
+function Profile(){
+    return(
+        <div className="layout">
+            <img src="assets/earn.jpg" alt="Profile"/>
+            <h1 className="name">Earn Pavika</h1>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Profile;
